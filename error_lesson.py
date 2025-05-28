8bee8d49810f4dbd334eb049e8a6051feb3833bf
yokozawa0701 <d.yoko.0701@gmail.com>
2025-05-28 23:58:15 +0900

    add

--- error_lesson.py
+++ error_lesson.py
@@ -1,9 +1,9 @@
 print("Hello, World!!")
 
-numbers = range(1, 10)
+numbers = range(1, 10)　
 
 for i in numbers:
     if i % 2 == 0:
-        print(f"{i} は偶数です")
+        print(f"{i} は偶数です")　
     else:
-        print(f"{i} は奇数です")
+        print(f"{i} は奇数です")　
