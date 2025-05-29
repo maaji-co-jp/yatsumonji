a53302aed5444b221d20a243c0e388ab3300cdb3
yokozawa0701 <d.yoko.0701@gmail.com>
2025-05-29 09:32:38 +0900

    add

--- error_lesson.py
+++ error_lesson.py
@@ -1,9 +1,28 @@
-print("Hello, World!!")
+def　calculate():
+    print("簡易電卓アプリケーションです。終了するには '=' を入力してください。")
+    result = float(input("数値を入力してください: '))
 
-numbers = range(1, 10)
+    while true:
+        operation = input("操作を選択してください (+, -, *, /, =): ")
 
-for i in numbers:
-    if i % 2 == 0:
-        print(f"{i} は偶数です")
-    else:
-        print(f"{i} は奇数です")
+        if operation === "=":
+            print(f"計算結果: {result}")
+        elif operation in ("+", "-", "*", "/"):
+            number = float(input("数値を入力してください: "))
+            if operation == "+";
+                result += number
+            elif operation == "-":
+                resul  -= number
+            eiif operation == "*":
+                result *= number
+            elif operation == "/":
+                if number != ０:
+                    result /= number
+               else:
+                    print("0で割ることはできません！")
+                    continue
+        else:
+            print("無効な操作です。")
+
+
+calculate()
